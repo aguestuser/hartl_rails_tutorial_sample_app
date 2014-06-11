f7ffe4d776b50e5e2d41a5eb1dd96a2476e2065f
Austin Guest <guest.austin@gmail.com>
2014-06-10 21:53:39 -0400

    add db migration w/ password fields to repo (oops), add tests for email downcasing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,6 +63,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when email address has mixed case" do
+    let(:mixed_case_email) { 'Foo@ExAmPlE.CoM' }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before do
       @user = User.new(name: 'Example User', email: 'user@example.com', password: '', password_confirmation: '')

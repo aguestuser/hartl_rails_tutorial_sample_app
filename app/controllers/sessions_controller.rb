9c2bd266d7dd1b1d800f8cbdc5a13eda7f2c1b33
Austin Guest <guest.austin@gmail.com>
2014-06-20 18:21:12 -0400

    correct typo in application.js, add sign_in & redirect_to user methods to session#create, modify application nav bar to include new menu for signed in users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # sign in user & redirect to user's show page
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'

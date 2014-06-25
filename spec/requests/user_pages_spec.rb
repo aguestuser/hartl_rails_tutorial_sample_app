718a40abc0586d8c5896b6dd1011a900d6cc928d
Austin Guest <guest.austin@gmail.com>
2014-06-25 19:41:27 -0400

    create edit user page and link to it from 'settings' option in 'account' nav bar dropdown

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -56,4 +56,23 @@ describe "UserPages" do
       end
     end
   end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href:'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid input" do
+      before { click_button "Save changes" }
+
+      it { should have_error_message('Invalid') }
+    end
+
+  end
+
 end

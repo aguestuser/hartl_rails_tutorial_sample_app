95f67e18fb3676cdca38e5833bd9c8b9451fd384
Austin Guest <guest.austin@gmail.com>
2014-06-26 00:42:35 -0400

    add name and email sequences to user factory definition

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name 'Foo Bar'
-    email 'foo.bar@example.com'
+    sequence(:name) { |n| "Person #{n}"}
+    sequence(:name) { |n| "person_#{n}@example.com"}
     password 'foobar'
     password_confirmation 'foobar'
   end  

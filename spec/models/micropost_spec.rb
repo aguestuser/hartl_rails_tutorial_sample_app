35a649e8ff0337ac55413734ee7f1b3e9751575c
Austin Guest <guest.austin@gmail.com>
2014-06-26 18:47:35 -0400

    add validation to Micropost model

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -2,14 +2,22 @@ require 'spec_helper'
 
 describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
-  before do
-    @micropost = Micropost.new(content: 'Lorem ipsum', user_id: user.id)
-  end
+  before { @micropost = Micropost.new(content: 'Lorem ipsum', user_id: user.id) }
 
   subject { @micropost }
 
   describe "attributes" do
     it { should respond_to(:content) }
     it { should respond_to(:user_id) }    
+  end
+
+  describe "validation" do
+
+    it { should be_valid }
+
+    describe "when user_id is not present" do
+      before { @micropost.user_id = nil }
+      it { should_not be_valid }
+    end
   end
 end

037f62fea715a1ab88398443095145176f06daef
Austin Guest <guest.austin@gmail.com>
2014-06-25 22:17:55 -0400

    create users index restricted to signed in users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,6 +15,7 @@ describe "Authentication" do
       before {signin(user)}
 
       it { should have_title(user.name) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
@@ -51,7 +52,7 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
 
-        describe "when signing in to access the protected page" do
+        describe "when signing in to access a protected page" do
           before do
             visit edit_user_path(user)
             fill_in 'Email',    with: user.email
@@ -65,6 +66,12 @@ describe "Authentication" do
             end
           end
         end
+
+        describe 'when visiting the user index' do 
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
       end
     end
 

d3a7484de952b987554a833a81f88e49848252cb
Austin Guest <guest.austin@gmail.com>
2014-06-26 15:54:04 -0400

    add inspection of request object to layouts/_debug_data partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user_cant_signup, only: [:create, :new]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -65,5 +66,9 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def signed_in_user_cant_signup
+      redirect_to root_url if signed_in?
     end
 end

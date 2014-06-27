fac8d8149887b3e572c025b75587c5d0f131c2b3
Austin Guest <guest.austin@gmail.com>
2014-06-27 19:55:36 -0400

    add delete link to homepage feed and profile page post list

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -10,7 +10,7 @@ describe "MicropostPages" do
   describe "micropost creation from home page" do
     before { visit root_path }
 
-    describe "with valid input" do
+    describe "with invalid input" do
       it "should not create a micropost" do
         expect { click_button 'Post' }.not_to change(Micropost, :count)
       end
@@ -19,7 +19,6 @@ describe "MicropostPages" do
         before { click_button 'Post' }
         it { should have_an_error_message }
       end
-
     end
 
     describe "with valid input" do
@@ -29,4 +28,16 @@ describe "MicropostPages" do
       end
     end
   end  
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost " do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end

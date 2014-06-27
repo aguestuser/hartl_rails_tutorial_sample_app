7ce8f1eba6d9d2895a9469df97e5f99b2ab2d49d
Austin Guest <guest.austin@gmail.com>
2014-06-26 20:00:57 -0400

    add microposts to User#show page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -42,17 +42,24 @@ describe "UserPages" do
           expect { click_first_delete_link }.to change(User, :count).by(-1)
         end
         it { should_not have_delete_link_for_user(admin) }
-        end
       end
     end
   end
 
   describe 'profile page' do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: 'Foo') }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: 'Bar') }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe 'signup page' do
